0f1f9cca311ca458d78ac8a50e43ea7d6e9038cd
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-13 23:01:29 +0530

    teacher model

---
+++ models/teacherModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { emailRegex } from "../utils/validators";
+
+const teacherSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "Please enter your first name"],
+    },
+    lastName: String,
+    LegalName: { type: String, required: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (email) {
+                return emailRegex.test(email);
+            },
+            message: "Invalid email",
+        },
+    },
+    password: {
+        type: String,
+        require: [true, "Please entere your password"],
+        minLength: [8, "Password must be atleast 8 characters"],
+        select: false,
+    },
+    avatarURL: String,
+    role: { type: String, default: "teacher" },
+    isVerified: { type: Boolean, default: false },
+    isApproved: { type: Boolean, default: false },
+    coursesCreated: [{ courseId: String }],
+    isBlocked: { type: Boolean, default: false },
+
+    phoneNo: {type: Number},
+    country: String,
+    biography: String,
+
+    education:String,
+    college: String,
+    qualification: String,
+    qualificationProof: String,
+    remark: String
+},{timestamps: true});
