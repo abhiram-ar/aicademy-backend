44a16736ca6e31714dadd4a4d002eac83a437af0
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-14 13:31:54 +0530

    modified: session schema, added roles

--- models/sessionModel.js
+++ models/sessionModel.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const sessionSchema = new mongoose.Schema({
-    userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    role: { type: String, required: true, enum: ["user", "teacher", "admin"] },
+    userId: { type: mongoose.Types.ObjectId, required: true },
     email: { type: String, required: true },
     refreshToken: { type: String, required: true },
     createdAt: { type: Date, default: Date.now(), index: { expires: "7d" } },
