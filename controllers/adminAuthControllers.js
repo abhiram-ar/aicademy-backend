cc1b5b7d8168713ad34e1d6735f99ee38efa7398
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-15 16:48:57 +0530

    admin access token upadate royte

--- controllers/adminAuthControllers.js
+++ controllers/adminAuthControllers.js
@@ -2,6 +2,7 @@ import adminModel from "../models/adminModel.js";
 import { logErrorMessage, logWarning } from "../utils/log.js";
 import { createAccessToken, createRefershToken } from "../utils/jwt.js";
 import sessionModel from "../models/sessionModel.js";
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
     try {
@@ -118,3 +119,116 @@ export const login = async (req, res) => {
             .json({ success: false, message: "login failed" });
     }
 };
+
+//get new accessToken - refersh token
+export const updateAccessToken = async (req, res) => {
+    const { refreshJWT } = req.cookies;
+    if (!refreshJWT) {
+        logWarning("updateAccesToken: cannot find refersh token in cookies");
+        return res
+            .status(401)
+            .json({ success: false, message: "No refresh token in cookies" });
+    }
+
+    jwt.verify(
+        refreshJWT,
+        process.env.REFRESH_TOKEN_SECRET,
+        async (error, decoded) => {
+            if (error) {
+                logErrorMessage("error while verifying refresh token");
+                logErrorMessage(error.message);
+                return res.status(400).json({
+                    success: false,
+                    message: "error while verifying refresh token",
+                });
+            }
+
+            const sessionDetails = await sessionModel.findOne({
+                refreshToken: refreshJWT,
+            });
+
+            if (!sessionDetails) {
+                logWarning(
+                    "refresh token does not exist in DB, requesting client to clear cookies"
+                );
+                res.clearCookie("refreshJWT", {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: "Lax",
+                });
+                return res.status(403).json({
+                    success: false,
+                    message: "admin session does't exist anymore",
+                });
+            }
+
+            const admin = await adminModel.findById(decoded.adminId);
+            if (!admin.isActive) {
+                logWarning("admin is inactive, cannot create new access token");
+                logWarning("requesting client to clear cookies");
+                res.clearCookie("refreshJWT", {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: "Lax",
+                });
+                return res.status(403).json({
+                    success: false,
+                    message:
+                        "admin is inactive. Cannot create new access token, requested to clear cookie",
+                });
+            }
+            
+            const newAccessToken = createAccessToken({
+                adminId: admin._id,
+                username: admin.firstName,
+                role: admin.role,
+            });
+
+
+
+            res.status(200).json(newAccessToken);
+        }
+    );
+};
+
+//logout admin
+export const logout = async (req, res) => {
+    try {
+        const { refreshJWT } = req.cookies;
+        if (!refreshJWT) {
+            logWarning("logout: cannot find refresh token in cookies");
+            return res.status(204).send();
+        }
+
+        let result = await sessionModel.deleteOne({ refreshToken: refreshJWT });
+
+        res.clearCookie("refreshJWT", {
+            httpOnly: true,
+            secure: false,
+            sameSite: "Lax",
+        });
+
+        if (result.deletedCount === 0) {
+            logWarning("logout: No session to delete");
+            return res.status(200).json({
+                success: true,
+                message:
+                    "cannot find session to logout, client is requested to clear the cookie",
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: "teacher logged out successfully",
+        });
+    } catch (error) {
+        logErrorMessage("error while logging out teacher");
+        logErrorMessage(error.message);
+        console.log(error);
+        res.status(400).json({
+            success: false,
+            messsage: "error while logging out teacher",
+        });
+    }
+};
+
+
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,10 +1,11 @@
-import { login, register } from "./../controllers/adminAuthControllers.js";
+import { login, register, updateAccessToken } from "./../controllers/adminAuthControllers.js";
 import express from "express";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/auth/register", register);
 adminRouter.post("/auth/login", login);
+adminRouter.post("/auth/refresh", updateAccessToken);
 
 
 
