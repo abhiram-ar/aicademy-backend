2fc72eef8f44c74f27f3e07769b7a833bea862f0
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-17 17:07:48 +0530

    added social auth for teachers

--- controllers/socialAuth.js
+++ controllers/socialAuth.js
@@ -3,6 +3,7 @@ import { client } from "../services/googleAuth.js";
 import userModel from "../models/userModel.js";
 import { createAccessToken, createRefershToken } from "../utils/jwt.js";
 import sessionModel from "../models/sessionModel.js";
+import teacherModel from "../models/teacherModel.js";
 
 export const googleAuth = async (req, res) => {
     try {
@@ -13,7 +14,6 @@ export const googleAuth = async (req, res) => {
                 message: "credentials not found",
             });
         }
-
         const ticket = await client.verifyIdToken({
             idToken: credentials,
             audience: process.env.GOOGLE_CLIENT_ID,
@@ -22,6 +22,8 @@ export const googleAuth = async (req, res) => {
 
         if (role === "user") {
             handleUser(req, res, payload);
+        } else if (role === "teacher") {
+            handleTeacher(req, res, payload);
         } else {
             logWarning("Invalid role for google auth");
             return res
@@ -104,5 +106,80 @@ const handleUser = async (req, res, payload) => {
         return res
             .status(400)
             .json({ success: false, message: "Error while user google auth" });
+    }
+};
+
+const handleTeacher = async (req, res, payload) => {
+    try {
+        let teacher = await teacherModel.findOne({ email: payload.email });
+
+        if (!teacher) {
+            logWarning(
+                "Google auth: teacher does not exist, trying to create new teacher"
+            );
+            teacher = await teacherModel.create({
+                email: payload.email,
+                firstName: payload.name,
+                avatarURL: payload.picture,
+            });
+            logSuccess("Google auth: new user created sucessfully");
+        }
+
+        if (teacher.isBlocked) {
+            logWarning("teacher is blocked, cannot login");
+            return res.status(403).json({
+                success: false,
+                message: "teacher is blocked. Contact admin",
+            });
+        }
+
+        const tokenPayload = {
+            userId: teacher._id,
+            username: teacher.firstName,
+            role: teacher.role,
+        };
+
+        const accessToken = createAccessToken({
+            ...tokenPayload,
+            isApproved: teacher.isApproved,
+        });
+        const refreshToken = createRefershToken(tokenPayload);
+
+        //save refreshtoken in session DB
+        await sessionModel.create({
+            role: teacher.role,
+            userId: teacher._id,
+            email: teacher.email,
+            refreshToken: refreshToken,
+        });
+
+        res.cookie("refreshJWT", refreshToken, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "Lax",
+            maxAge: 60 * 60 * 1000, //1hr
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "login successful",
+            token: accessToken,
+            user: {
+                _id: teacher._id,
+                firstName: teacher.firstName,
+                email: teacher.email,
+                isApproved: teacher.isApproved,
+            },
+        });
+    } catch (error) {
+        logErrorMessage(`error while teacher google auth`);
+        logErrorMessage(error.message);
+        console.log(error);
+        return res
+            .status(400)
+            .json({
+                success: false,
+                message: "Error while teacher google auth",
+            });
     }
 };
