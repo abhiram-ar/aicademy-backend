b87b008ca3ca1b8c7c12ed2b8f373f4a0d5fb5b4
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-19 14:40:13 +0530

    added types to user and teacher schema

--- models/userModel.ts
+++ models/userModel.ts
@@ -2,7 +2,23 @@ import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import { emailRegex } from "../utils/validators.ts";
 
-const userSchema = new Schema(
+
+interface IUser extends Document {
+    firstName: string;
+    lastName?: string;
+    email: string;
+    googleAuth?: boolean;
+    password?: string;
+    avatarURL?: string;
+    role: string;
+    isVerified: boolean;
+    coursesBought: { courseId: string }[];
+    isBlocked: boolean;
+    comparePassword(enteredPassword: string): Promise<boolean>;
+}
+
+
+const userSchema = new Schema<IUser>(
     {
         firstName: {
             type: String,
