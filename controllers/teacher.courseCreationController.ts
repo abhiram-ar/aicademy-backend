e33426eebd064971dbe7c8545fc6a0d1c52e4c16
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-21 20:20:48 +0530

    added fetch full course details controlller

--- controllers/teacher.courseCreationController.ts
+++ controllers/teacher.courseCreationController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import courseModel from "../models/course.model";
-import { logErrorMessage } from "../utils/log";
+import { logErrorMessage, logWarning } from "../utils/log";
 
 // Extend the Request interface
 export interface TRequest extends Request {
@@ -32,6 +32,65 @@ export const createDraft = async (req: TRequest, res: Response) => {
         res.status(400).json({
             success: false,
             messsage: "error while creating course draft",
+        });
+    }
+};
+
+export const getCourseDraftList = async (
+    req: TRequest,
+    res: Response
+): Promise<any> => {
+    try {
+        const draftList = await courseModel.find(
+            {
+                createdBy: req.user.teacherId,
+                courseState: "draft",
+            },
+            { title: 1 }
+        );
+
+        console.log(draftList);
+        return res.status(200).json(draftList);
+    } catch (error) {
+        logErrorMessage("error while fetching teacher's draft list");
+        logErrorMessage(error.message);
+        console.log(error);
+        return res.status(400).json({
+            success: false,
+            message: "error while fething draft course list",
+        });
+    }
+};
+
+export const getCourseDetails = async (req: TRequest, res: Response):Promise<any> => {
+    try {
+        const { courseId } = req.body;
+        if (!courseId) {
+            logWarning("courseID not provided");
+            return res
+                .status(404)
+                .json({ success: false, message: "CourseId not provided" });
+        }
+
+        const courseDetails = await courseModel.find({
+            createdBy: req.user.teacherId,
+            _id: courseId,
+        });
+
+        return res
+            .status(200)
+            .json({
+                success: true,
+                messsage: "course details sucessfully fetched",
+                courseDetails,
+            });
+    } catch (error) {
+        logErrorMessage("Error while Fetching a course details");
+        logErrorMessage(error.message);
+        console.log(error);
+        return res.status(400).json({
+            success: false,
+            message: "error while fetching course details",
         });
     }
 };
--- routes/courseRouter.ts
+++ routes/courseRouter.ts
@@ -2,17 +2,32 @@ import Express from "express";
 import { generatePresignedURL } from "../controllers/teacher.courseManagementControllers";
 import {
     createDraft,
+    getCourseDetails,
+    getCourseDraftList,
     TRequest,
 } from "./../controllers/teacher.courseCreationController";
 import { authorizedRoles, isAuthenticated } from "../middlewares/auth";
 
 const courseRouter = Express.Router();
 
+
+
+
+
+//protected rotues
+courseRouter.use(isAuthenticated, authorizedRoles("teacher"));
+
 courseRouter.post(
     "/create/draft",
-    isAuthenticated,
-    authorizedRoles("teacher"),
     (req, res) => createDraft(req as TRequest, res) //ts type assertion
+);
+
+courseRouter.get("/draft-list", (req, res) =>
+    getCourseDraftList(req as TRequest, res)
+);
+
+courseRouter.get("/full-details", (req, res) =>
+    getCourseDetails(req as TRequest, res)
 );
 
 courseRouter.post("/upload/presignedurl", generatePresignedURL);
