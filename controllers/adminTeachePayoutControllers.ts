daa883f48cdb9d595c233b4df55814f4b5ebe3ef
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-11 14:20:25 +0530

    getpayout list controller

--- controllers/adminTeachePayoutControllers.ts
+++ controllers/adminTeachePayoutControllers.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from "express";
 import { logErrorMessage } from "../utils/log";
+import payoutModel from "./../models/payoutModel";
 
-export const getPayoutList = (req: Request, res: Response) => {
+export const getPayoutList = async (req: Request, res: Response) => {
     try {
+        const { page = 1, limit = 10 } = req.query;
+        const skip = (parseInt(page as string) - 1) * parseInt(limit as string);
+
+        // fetch all payout transaction other than verifiaction transactions
+        const result = await payoutModel
+            .find({
+                message: {
+                    $ne: "Verifiacation Refund",
+                },
+            })
+            .skip(skip)
+            .limit(parseInt(limit as string))
+            .populate({ path: "to", select: "legalName" })
+            .sort({ isApproved: 1, createdAt: 1 });
+
+        const totalEntries = await payoutModel.countDocuments({
+            message: { $ne: "Verifiacation Refund" },
+        });
+
         res.status(200).json({
             success: false,
             message: "payout list successfully fethched",
+            length: result.length,
+            pages: Math.ceil(totalEntries / parseInt(limit as string)),
+            payoutList: result,
         });
     } catch (error) {
         logErrorMessage("error while fetching payout  list");
