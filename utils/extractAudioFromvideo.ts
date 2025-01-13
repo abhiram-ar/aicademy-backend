4de415e4770d2a69850ea24d87cf14ef20e37225
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-13 21:50:28 +0530

    audio extraction

---
+++ utils/extractAudioFromvideo.ts
@@ -0,0 +1,59 @@
+import ffmpeg from "fluent-ffmpeg";
+import path from "path";
+import { logErrorMessage, logSuccess } from "./log";
+import fs from "fs/promises";
+
+interface AudioOptions {
+    format?: string; // Output format (mp3, wav, aac)
+    bitrate?: string;
+    sampleRate?: string;
+    channels?: number;
+}
+
+export const extractAudio = async (
+    inputPath: string,
+    options: AudioOptions = {}
+) => {
+    const {
+        format = "opus", // compressed but, good for realtime audio processing
+        bitrate = "32k", // opus retain good speech quality even at low bit rate
+        sampleRate = "16000", // 16k is good for speech to text anything above this give diminishing returns
+        channels = 1, // since we are doing speech recognition dual channel is not mandatory
+    } = options;
+
+    const outputDir = path.dirname(inputPath);
+    await fs.mkdir(outputDir, { recursive: true });
+    const outputPath =
+        path.join(outputDir, path.parse(inputPath).name) + "." + format;
+
+    return new Promise((resolve, reject) => {
+        console.log("Starting to Audio Extraction.....");
+        ffmpeg(inputPath)
+            .toFormat(format)
+            .withAudioBitrate(bitrate)
+            .withAudioFrequency(parseInt(sampleRate))
+            .withAudioChannels(channels)
+            .output(outputPath)
+
+            // handling reformat
+            .on("progress", (progress) =>
+                console.log(`Audio extraction: ${progress.percent}% done`)
+            )
+            .on("end", () => {
+                logSuccess("Audio extraction complete");
+                resolve(outputPath);
+            })
+            .on("error", (error) => {
+                logErrorMessage("error while extracting audio");
+                console.log(error);
+                reject(error);
+            })
+            .run();
+    });
+};
+
+// const result = extractAudio(
+//     "/home/abhiram/Bootcamp/week-10/AIcademy/backend/temp/downloads/Elon.mp4"
+// )
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log(error));
