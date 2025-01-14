9fb993f9a85dd6d4ad6e1144d4f59f9d73e0a942
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-14 16:50:48 +0530

    docs

--- services/AI_Preprocessing_service/extractAudioFromvideo.ts
+++ services/AI_Preprocessing_service/extractAudioFromvideo.ts
@@ -1,7 +1,7 @@
-import ffmpeg from "fluent-ffmpeg";
-import path from "path";
-import { logErrorMessage, logSuccess } from "../../utils/log";
-import fs from "fs/promises";
+import ffmpeg from 'fluent-ffmpeg';
+import path from 'path';
+import { logErrorMessage, logSuccess } from '../../utils/log';
+import fs from 'fs/promises';
 
 interface AudioOptions {
     format?: string; // Output format (mp3, wav, aac)
@@ -10,24 +10,20 @@ interface AudioOptions {
     channels?: number;
 }
 
-export const extractAudio = async (
-    inputPath: string,
-    options: AudioOptions = {}
-) => {
+export const extractAudio = async (inputPath: string, options: AudioOptions = {}) => {
     const {
-        format = "opus", // compressed but, good for realtime audio processing
-        bitrate = "32k", // opus retain good speech quality even at low bit rate
-        sampleRate = "16000", // 16k is good for speech to text anything above this give diminishing returns
+        format = 'opus', // compressed but, good for realtime audio processing
+        bitrate = '32k', // opus retain good speech quality even at low bit rate
+        sampleRate = '16000', // 16k is good for speech to text anything above this give diminishing returns
         channels = 1, // since we are doing speech recognition dual channel is not mandatory
     } = options;
 
     const outputDir = path.dirname(inputPath);
-    await fs.mkdir(outputDir, { recursive: true });
-    const outputPath =
-        path.join(outputDir, path.parse(inputPath).name) + "." + format;
+    await fs.mkdir(outputDir, { recursive: true }); //make sure the dir exists
+    const outputPath = path.join(outputDir, path.parse(inputPath).name) + '.' + format;
 
     return new Promise((resolve, reject) => {
-        console.log("Starting to Audio Extraction.....");
+        console.log('Starting to Audio Extraction.....');
         ffmpeg(inputPath)
             .toFormat(format)
             .withAudioBitrate(bitrate)
@@ -36,17 +32,15 @@ export const extractAudio = async (
             .output(outputPath)
 
             // handling reformat
-            .on("progress", (progress) =>
-                console.log(
-                    `Audio extraction: ${progress.percent?.toFixed(2)}% done`
-                )
+            .on('progress', (progress) =>
+                console.log(`Audio extraction: ${progress.percent?.toFixed(2)}% done`)
             )
-            .on("end", () => {
-                logSuccess("Audio extraction complete");
+            .on('end', () => {
+                logSuccess('Audio extraction complete');
                 resolve(outputPath);
             })
-            .on("error", (error) => {
-                logErrorMessage("error while extracting audio");
+            .on('error', (error) => {
+                logErrorMessage('error while extracting audio');
                 console.log(error);
                 reject(error);
             })
