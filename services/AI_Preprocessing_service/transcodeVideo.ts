87fe17b7b205dd21f1f165ddf0973c8cf27cf121
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-20 21:16:40 +0530

    fixed masterfile incompatability issue

--- services/AI_Preprocessing_service/transcodeVideo.ts
+++ services/AI_Preprocessing_service/transcodeVideo.ts
@@ -104,16 +104,22 @@ class TranscodingJob {
     }
 
     createMasterPlaylist() {
-        let playlist = "#EXTM3U\n";
-        playlist += "#EXT-X-VERSION:3\n";
-
-        RESOLUTIONS.forEach((resolution) => {
-            playlist += `EXT-X-STREAM-INF:BANDWIDTH=${
-                parseInt(resolution.bitrate) * 1000
-            }, RESOLUTION=${this.calculateWidth(resolution.height)}x${resolution.height}\n`;
-
-            playlist += `${resolution.name}/playlist.m3u8\n`;
-        });
+        const playlist = [
+            "#EXTM3U",
+            "#EXT-X-VERSION:3",
+            "",
+            // 1080p variant
+            '#EXT-X-STREAM-INF:BANDWIDTH=5000000,RESOLUTION=1920x1080,CODECS="avc1.42e01e,mp4a.40.2",AUDIO="audio-aac-128k"',
+            "1080p/playlist.m3u8",
+            "",
+            // 720p variant
+            '#EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720,CODECS="avc1.42e01e,mp4a.40.2",AUDIO="audio-aac-128k"',
+            "720p/playlist.m3u8",
+            "",
+            // 320p variant
+            '#EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=568x320,CODECS="avc1.42e01e,mp4a.40.2",AUDIO="audio-aac-128k"',
+            "320p/playlist.m3u8",
+        ].join("\n");
 
         return playlist;
     }
