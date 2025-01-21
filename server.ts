bec25d068a1ed936dc10a9bb2cdaa8495a831bb3
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-21 16:51:42 +0530

    string format

--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config';
-import app from './app.ts';
-import connectDB from './config/mongoose.ts';
-import { WebSocketServer, WebSocket } from 'ws';
-import { authenticateWSClient } from './Websocket/authenticateClient.ts';
-import { onPreSocketError } from './Websocket/onPreSocketError.ts';
-import { IncomingMessage } from 'http';
-import { logSuccess } from './utils/log.ts';
-import { handleRAGQuestion } from './Websocket/handleRAGQuestion.ts';
+import "dotenv/config";
+import app from "./app.ts";
+import connectDB from "./config/mongoose.ts";
+import { WebSocketServer, WebSocket } from "ws";
+import { authenticateWSClient } from "./Websocket/authenticateClient.ts";
+import { onPreSocketError } from "./Websocket/onPreSocketError.ts";
+import { IncomingMessage } from "http";
+import { logSuccess } from "./utils/log.ts";
+import { handleRAGQuestion } from "./Websocket/handleRAGQuestion.ts";
 
 connectDB();
 
@@ -17,37 +17,37 @@ const httpServer = app.listen(PORT, () => {
 
 const wss = new WebSocketServer({ noServer: true });
 
-httpServer.on('upgrade', async (req, socket, head) => {
+httpServer.on("upgrade", async (req, socket, head) => {
     // listen for error events which might occur during connection updrade process
-    socket.on('error', (error) => onPreSocketError(error, socket));
+    socket.on("error", (error) => onPreSocketError(error, socket));
 
     let client: string | undefined;
     try {
         client = await authenticateWSClient(req);
-        if (!client) throw new Error('no client found');
+        if (!client) throw new Error("no client found");
     } catch (error) {
         console.log(error);
-        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+        socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
         socket.destroy();
         return;
     }
 
     wss.handleUpgrade(req, socket, head, (ws) => {
-        socket.removeListener('error', onPreSocketError);
-        wss.emit('connection', ws, req, client);
+        socket.removeListener("error", onPreSocketError);
+        wss.emit("connection", ws, req, client);
     });
 });
 
-wss.on('connection', (ws: WebSocket, request: IncomingMessage, connectingClientUserId: string) => {
+wss.on("connection", (ws: WebSocket, request: IncomingMessage, connectingClientUserId: string) => {
     logSuccess(`New connection established: userId:${connectingClientUserId}`);
 
-    ws.on('error', (err) => {
-        console.error('Websocket error', { message: err.message, client: connectingClientUserId });
+    ws.on("error", (err) => {
+        console.error("Websocket error", { message: err.message, client: connectingClientUserId });
     });
 
-    ws.on('message', (msg) => handleRAGQuestion(msg, ws));
+    ws.on("message", (msg) => handleRAGQuestion(msg, ws));
 
-    ws.on('close', () => {
+    ws.on("close", () => {
         console.log(`Closing websocket connection for {userId:${connectingClientUserId}}`);
         //furthur cleanup if necessary
     });
